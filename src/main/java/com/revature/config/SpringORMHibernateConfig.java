ee2b31f8cd7d68eb511f0c579ab06d71b1cd928e
Revature-Josh <81986637+Revature-Josh@users.noreply.github.com>
2021-05-23 15:11:34 -0400

    Update SpringORMHibernateConfig.java
    Reset to online database

--- src/main/java/com/revature/config/SpringORMHibernateConfig.java
+++ src/main/java/com/revature/config/SpringORMHibernateConfig.java
@@ -24,10 +24,9 @@ public class SpringORMHibernateConfig {
 		
 		// Please use environment variables in a real app
 		dataSource
-		.setUrl("jdbc:mariadb://localhost:3306/project_2_database");
-		dataSource.setUsername("root");
-		dataSource.setPassword("rootuserpw");
-		
+		.setUrl("jdbc:mariadb://database-1.c0hosdsgeanm.us-east-1.rds.amazonaws.com:3306/amazondb");
+		dataSource.setUsername("admin");
+		dataSource.setPassword("password");
 		return dataSource;
 	}
 	
